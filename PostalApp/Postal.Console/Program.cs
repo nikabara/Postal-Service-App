f7f20d0b366c0fe7b9ff909c56e3fb08b81da1eb
nikoloz baratashvili <n.baratashvili@itstep.ge>
2022-11-15 20:36:22 +0400

    major testing

---
+++ PostalApp/Postal.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+using Postal.Services.Interfaces;
+using Postal.Library;
+using Microsoft.Data.SqlClient;
+using Postal.Services;
+
+SqlDataConnector CSC = new SqlDataConnector();
+
+var result = CSC.GetUsers();
+
+Console.ReadLine();
