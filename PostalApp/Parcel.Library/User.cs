0531041a2fe72527ab9ba6772c2cbcc1e4a91598
nikoloz baratashvili <n.baratashvili@itstep.ge>
2022-11-15 20:36:11 +0400

    Added Fullname

--- PostalApp/Parcel.Library/User.cs
+++ PostalApp/Parcel.Library/User.cs
@@ -5,6 +5,7 @@
         public int UserId { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+        public string? FullName { get; set; }
         public byte Age { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
