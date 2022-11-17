f44825351d5236167c26a7a45a2b8651880ab254
nikoloz baratashvili <n.baratashvili@itstep.ge>
2022-11-17 19:19:45 +0400

    added user interface (ui) and backend functionalitiy with sql db

--- PostalApp/PostalApp.UI/Form1.cs
+++ PostalApp/PostalApp.UI/Form1.cs
@@ -1,10 +1,59 @@
+using Postal.Library;
+using System.Data;
+using Postal.Services.Interfaces;
+using Microsoft.Data.SqlClient;
+using Postal.Services;
+
 namespace PostalApp.UI
 {
-    public partial class Form1 : Form
+    public partial class SignUpForm : Form
     {
-        public Form1()
+        public SignUpForm()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlDataConnector dataConnection = new SqlDataConnector();
+            dataConnection.InsertUser(new User
+            {
+                FirstName = NameInput.Text,
+                LastName = LastNameInput.Text,
+                Age = byte.Parse(AgeInput.Text),
+                Email = EmailInput.Text,
+                PhoneNumber = PhoneNumberInput.Text
+            });
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
